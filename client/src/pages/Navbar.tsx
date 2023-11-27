84a0f147cf81c281f651efd97f9c2a05bb56da1c
krishvvk33 <vnrao95vk@gmail.com>
2023-11-27 10:45:29 +0530

    routes-to-navbar

--- client/src/pages/Navbar.tsx
+++ client/src/pages/Navbar.tsx
@@ -2,17 +2,30 @@ import React from "react";
 
 
 function Navbar() {
+
   
   return (
     <nav className="sticky top-0 bg-white border-b-2 border-slate-300 p-3 ">
       <div className="container mx-auto flex items-center">
-      <img className="gdsc_logo" src="https://cdn-images-1.medium.com/max/578/1*vZVM7utCuRiZ6-HDsNeYUA@2x.png" width="50" height="50" /> 
+        <a href="/">
+        <img className="gdsc_logo" src="https://cdn-images-1.medium.com/max/578/1*vZVM7utCuRiZ6-HDsNeYUA@2x.png" width="50" height="50"/>
+        </a>
+        <a href="/">
         <h1 className="text-[#868686] text-lg pl-5">GDSC VNRVJIET.</h1>
+        </a>
         <div className="flex ml-auto text-slate-600 ">
-        <button className="pl-5">Upcoming Events</button>
-        <button className="pl-5">Past events</button>
-          <button className="pl-5">Forum</button>
-          <button className="pl-5">Login</button>
+        <button className="pl-5" >
+          <a href="/Upcoming-events">Upcoming Events</a>
+        </button>
+        <button className="pl-5">
+          <a href="/Past-events">Past events</a>
+        </button>
+        <button className="pl-5">
+          <a href="/Forum" >Forum</a>
+        </button>
+          <button className="pl-5">
+            <a href="/Login">Login</a>
+          </button>
         </div>
       </div>
     </nav>

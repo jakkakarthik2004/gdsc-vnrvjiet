229c82d7ba93921de6613e7906e9cfe6038e4030
Rinku2002 <gangishettyrinku@gmail.com>
2023-12-03 00:23:55 +0530

    changes

--- client/src/Apis/events.ts
+++ client/src/Apis/events.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.REACT_APP_BACK_URL;
 
 export const createEvent = async (eventDto: any) => {
   try {

e14ce9123cadeeb779f81ad6f0b3c6fdc710ce28
jayasree <jgondipalle@gmail.com>
2024-03-02 10:55:17 +0530

    leaderboard changes

--- client/src/Apis/juries.ts
+++ client/src/Apis/juries.ts
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_BACK_URL;
 
-export const submitEval = async(dto:any)=>{
-    try {
-        const response = await axios.post(`${API_URL}/jury/create`, dto);
-        console.log(dto)
-        return response.data;
-      } catch (error) {
-        console.error("Error creating event:", error);
-        throw error;
-      }
+export const submitEval = async (dto: any) => {
+  try {
+    const response = await axios.post(`${API_URL}/jury/create`, dto);
+    console.log(dto);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating event:", error);
+    throw error;
+  }
+};
+
+export const getLeaderboard = async () => {
+  try {
+    const response = await axios.post(`${API_URL}/jury/get-metrics`);
+    return response.data;
+  } catch (error) {
+    console.error("Error getting Leaderboard", error);
+    throw error;
+  }
 };
--- server_node/APIs/juryApi.js
+++ server_node/APIs/juryApi.js
@@ -7,35 +7,45 @@ require("dotenv").config();
 
 juryApp.use(exp.json());
 
-juryApp.post("/create", expressAsyncHandler(async (request, response) => {
+juryApp.post(
+  "/create",
+  expressAsyncHandler(async (request, response) => {
     try {
-        let juryCollectionObject = await getDBObj("juryCollectionObject");
-        console.log(juryCollectionObject)
-        let { juryName, teamName, teamLead, ...metrics } = request.body;
-        let existingJury = await juryCollectionObject.findOne({ juryName });
-    
-        if (existingJury) {
-          await juryCollectionObject.updateOne(
-            { juryName },
-            { $set: { teamName, teamLead, metrics } }
-          );
-          response.send({ message: "Jury information updated" });
-        } else {
-          await juryCollectionObject.insertOne({ juryName, teamName, teamLead, metrics });
-          response.send({ message: "Jury created with evaluation" });
-        }
-      } catch (error) {
-        console.error("Error while posting eval data", error);
-        response.status(500).send({ error: "An error occurred" });
-      }
-    }));
-
-  juryApp.get("/get-metrics",
-    expressAsyncHandler(async (request, response) => {
       let juryCollectionObject = await getDBObj("juryCollectionObject");
-      let eval = await juryCollectionObject.find().toArray();
-      response.send({ message: "questions list", payload: eval });
-    })
-  );
+      console.log(juryCollectionObject);
+      let { juryName, teamName, teamLead, ...metrics } = request.body;
+      let existingJury = await juryCollectionObject.findOne({ juryName });
 
-  module.exports = juryApp;
+      if (existingJury) {
+        await juryCollectionObject.updateOne(
+          { juryName },
+          { $set: { teamName, teamLead, metrics } }
+        );
+        response.send({ message: "Jury information updated" });
+      } else {
+        await juryCollectionObject.insertOne({
+          juryName,
+          teamName,
+          teamLead,
+          metrics,
+        });
+        response.send({ message: "Jury created with evaluation" });
+      }
+    } catch (error) {
+      console.error("Error while posting eval data", error);
+      response.status(500).send({ error: "An error occurred" });
+    }
+  })
+);
+
+juryApp.get(
+  "/get-metrics",
+  expressAsyncHandler(async (request, response) => {
+    let juryCollectionObject = await getDBObj("juryCollectionObject");
+    let eval = await juryCollectionObject.find().toArray();
+    console.log(eval);
+    response.send({ message: "questions list", payload: eval });
+  })
+);
+
+module.exports = juryApp;

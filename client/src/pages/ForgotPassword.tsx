7c1b92144d9b4bbf181e37850994901e1e823849
Jahnavi <jahnavi.rallapalli2003@gmail.com>
2024-03-19 13:13:41 +0530

    UI for forgot password related pages

--- client/src/pages/ForgotPassword.tsx
+++ client/src/pages/ForgotPassword.tsx
@@ -43,36 +43,45 @@ function ForgotPassword() {
   };
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
+    <div className=" flex flex-col items-center">
+      
       {!otpSent ? (
-        <div>
+        <div className="mt-16 justify-center bg-gray-200 p-8 rounded ">
+          <h2 className="text-center text-2xl font-bold pb-3" >Forgot Password?</h2>
           <div>
             <label>Email:</label>
             <input
+              className="w-80 p-3 m-3 rounded"
               type="email"
               value={email}
+              placeholder="enter the email you registered with"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <button onClick={handleSendOtp}>Send OTP</button>
+          <button className=" w-24 bg-green-600 text-white font-bold rounded p-2 m-2 r hover:ring ring-green-400 ring-offset-2 transition" onClick={handleSendOtp}>Send OTP</button>
         </div>
       ) : (
-        <div>
+        <div className=" mt-16 justify-center flex flex-col bg-gray-200 rounded-xl p-10">
+          <h2 className="text-center text-2xl font-bold pb-3" >Verify your OTP</h2>
+          <div>
           <label>Enter OTP:</label>
-          <input
-            type="text"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-          />
-          <button onClick={handleVerifyOtp}>Verify OTP</button>
+            <input
+              className="rounded m-3 p-3"
+              type="text"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+            />
+          </div>
+          <button className="bg-blue-400 p-2 m-2 font-bold rounded hover:ring ring-blue-600 ring-offset-2 transition"  onClick={handleVerifyOtp}>Verify OTP</button>
         </div>
       )}
       {verified && (
-        <div>
-          <div>
+        <div className=" text-center mt-16 justify-center flex flex-col bg-gray-200 rounded-xl p-10">
+          <h1 className="text-center text-2xl font-bold pb-3">Set a new password :</h1>
+          <div className="">
             <label>New Password:</label>
             <input
+              className="rounded m-3 p-3"
               type="password"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
@@ -81,12 +90,15 @@ function ForgotPassword() {
           <div>
             <label>Confirm Password:</label>
             <input
+              className="rounded m-3 p-3"
               type="password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
-          <button onClick={handleResetPassword}>submit</button>
+          <button 
+          className="bg-green-500 p-2 m-2 font-bold rounded hover:ring ring-green-600 ring-offset-2 transition"
+          onClick={handleResetPassword}>submit</button>
         </div>
       )}
     </div>

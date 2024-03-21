f8a381af062c8e821dae5ddbb6d22e8a4989452f
Jahnavi <jahnavi.rallapalli2003@gmail.com>
2024-03-21 23:29:46 +0530

    fixed otp bug, added toasts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import Root from "./Root";
 import "./output.css";
 import Navbar from "./pages/Navbar";
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="bg-slate-50 min-h-screen text-slate-900">
-      <Navbar />
-      <Root />
-    </div>
+      <div className="bg-slate-50 min-h-screen text-slate-900">
+        <Navbar />
+        <Root />
+        <Toaster />
+      </div>
   );
 }
 

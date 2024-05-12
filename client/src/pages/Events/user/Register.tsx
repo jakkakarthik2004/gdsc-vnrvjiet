eccc36f065168683323ecd4a9f26c014493ccf22
srimanikantaBattu <srimanikantabattu04@gmail.com>
2024-05-12 11:08:02 +0530

    razorpay

--- client/src/pages/Events/user/Register.tsx
+++ client/src/pages/Events/user/Register.tsx
@@ -111,6 +111,7 @@ const PaymentGatewayRazorpay: React.FC = () => {
             </label>
           </div>
         </div>
+        {/* <div className=""></div> */}
         <div className="mt-3 ml-3">
           <button
             className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
